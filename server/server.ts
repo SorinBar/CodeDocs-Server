79403426c72b3f457064121918eef09aa36afcc5
Sorin Barbu <sbarbu38@yahoo.com>
2024-04-13 20:54:50 +0300

    added language option

--- server/server.ts
+++ server/server.ts
@@ -40,7 +40,7 @@ const SYSTEM_CONTEXT_DECISION: string =
   Function / Componenet / None
 `;
 
-const SYSTEM_CONTEXT_FUNCTION: string = 
+const SYSTEM_CONTEXT_FUNCTION_EN: string = 
 `
   Generate a documentation for a function containing:
     - name of the function
@@ -53,7 +53,7 @@ const SYSTEM_CONTEXT_FUNCTION: string =
   "{\"name\":\"the name of the function without async\",\"params\":{\"param1\":\"description1\",\"param2\":\"description2\"},\"description\":\"the whole function description\",\"usage\":\"how to use the function API\"}"
 `;
 
-const SYSTEM_CONTEXT_COMPONENT: string = 
+const SYSTEM_CONTEXT_COMPONENT_EN: string = 
 `
   Generate a documentation for a React component containing:
     - props with their description
@@ -63,6 +63,32 @@ const SYSTEM_CONTEXT_COMPONENT: string =
   Structure the response in a JSON compatible format such as:
   
   "{\"name\":\"the name of the component\",\"props\":{\"prop1\":\"description1\",\"prop2\":\"description2\"},\"description\":\"the whole component description\",\"usage\":\"how to use the component API\"}"
+`;
+
+const SYSTEM_CONTEXT_FUNCTION_RO: string = 
+`
+  Genereaza in romana o documentatie pentru o functie care sa contina:
+    - numele functie
+    - parametri cu descrierea
+    - descrierea functiei
+    - modul in care putem folosii API-ul functiei
+
+  Structureaza raspunsul intr-un JSON compatibil cu formatul:
+  
+  "{\"name\":\"numele functiei fara async\",\"params\":{\"parametrul1\":\"descrierea1\",\"parametrul2\":\"descrierea2\"},\"description\":\"descrierea intregii functiei\",\"usage\":\"cum se foloseste API functiei\"}"
+`;
+
+const SYSTEM_CONTEXT_COMPONENT_RO: string = 
+`
+  Genereaza in romana o documentatie pentru o componenta React care sa contina:
+    - numele componentei
+    - parametri cu descrierea
+    - descrierea componentei
+    - modul in care putem folosii API-ul componentei
+
+  Structureaza raspunsul intr-un JSON compatibil cu formatul:
+  
+  "{\"name\":\"numele componentei\",\"params\":{\"parametrul1\":\"descrierea1\",\"parametrul2\":\"descrierea2\"},\"description\":\"descrierea intregii componente\",\"usage\":\"cum se foloseste API componentei\"}"
 `;
 
 
@@ -114,11 +140,18 @@ export class Server {
       status: ResponseStatus.Success,
     };
     let response_str: string | null | undefined;
+    let system_context: string;
     
+    if (language == Language.En) {
+      system_context = SYSTEM_CONTEXT_FUNCTION_EN;
+    } else {
+      system_context = SYSTEM_CONTEXT_FUNCTION_RO;
+    }
+
     const GPT_response = await this.openai?.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [
-        {"role": "system", "content": SYSTEM_CONTEXT_FUNCTION},
+        {"role": "system", "content": system_context},
         {"role": "user", "content": data}
       ]
     });
@@ -147,11 +180,18 @@ export class Server {
       data: ""
     };
     let response_str: string | null | undefined;
-    
+    let system_context: string;
+
+    if (language == Language.En) {
+      system_context = SYSTEM_CONTEXT_COMPONENT_EN;
+    } else {
+      system_context = SYSTEM_CONTEXT_COMPONENT_RO;
+    }
+
     const GPT_response = await this.openai?.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [
-        {"role": "system", "content": SYSTEM_CONTEXT_COMPONENT},
+        {"role": "system", "content": system_context},
         {"role": "user", "content": data}
       ]
     });
