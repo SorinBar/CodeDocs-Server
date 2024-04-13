f5dc70db5a784fe4035844a92ffeeecfd0cca8bf
Sorin Barbu <sbarbu38@yahoo.com>
2024-04-13 18:31:40 +0300

    Done component

--- server/server.ts
+++ server/server.ts
@@ -26,6 +26,19 @@ const SYSTEM_CONTEXT_FUNCTION: string =
   "{\"params\":{\"param1\":\"description1\",\"param2\":\"description2\"},\"description\":\"the whole function description\",\"usage\":\"how to use the function API\"}"
 `;
 
+const SYSTEM_CONTEXT_COMPONENT: string = 
+`
+  Generate a documentation for a React component containing:
+    - props with their description
+    - description of the component
+    - usage containing the usage of the component API
+
+  Structure the response in a JSON compatible format such as:
+  
+  "{\"props\":{\"prop1\":\"description1\",\"prop2\":\"description2\"},\"description\":\"the whole component description\",\"usage\":\"how to use the component API\"}"
+`;
+
+
 @GenezioDeploy()
 export class Server {
   private openai: OpenAI | null = null;
@@ -52,9 +65,7 @@ export class Server {
     let response_str: string | null | undefined;
     
     const GPT_response = await this.openai?.chat.completions.create({
-      // the used model at the moment of writing this article
       model: "gpt-3.5-turbo",
-      // tells ChatGPT to rephrase the requestText
       messages: [
         {"role": "system", "content": SYSTEM_CONTEXT_FUNCTION},
         {"role": "user", "content": data}
@@ -63,7 +74,38 @@ export class Server {
 
     try {
       response_str = GPT_response?.choices[0].message.content;
-      console.log(response_str);
+      if (response_str) {
+        response.data = JSON.parse(response_str);
+      } else {
+        response.status = "error";
+        response.data = "";
+      }
+    } catch (error) {
+      console.log(error);
+      response.status = "error";
+      response.data = "";
+    }
+
+    return JSON.stringify(response);
+  }
+
+  async componentGenDoc(language: Language, data: string) {
+    let response: AIResponse = {
+      status: "success",
+      data: ""
+    };
+    let response_str: string | null | undefined;
+    
+    const GPT_response = await this.openai?.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {"role": "system", "content": SYSTEM_CONTEXT_COMPONENT},
+        {"role": "user", "content": data}
+      ]
+    });
+
+    try {
+      response_str = GPT_response?.choices[0].message.content;
       if (response_str) {
         response.data = JSON.parse(response_str);
       } else {
